bc4ac92ac0f4595e356e4baca7ca57da457cbf6c
Shpilevskayaas <shpilevskayaas@ya.ru>
2021-10-27 01:07:56 +0300

    main_order+popup

--- js/main.js
+++ js/main.js
@@ -8,4 +8,18 @@ $(document).ready(function() {
      });
    });
  });
- 
+$(document).ready(function() {   
+    $('a[name=modal]').click(function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href');
+    var winH = $(window).height();
+    var winW = $(window).width();
+    $(id).css('top',  winH/2-$(id).height()/2);
+    $(id).css('left', winW/2-$(id).width()/2);
+    $(id).fadeIn(600); 
+    });
+    $('.window .close').click(function (e) { 
+    e.preventDefault();
+    $('.window').hide();
+    }); 
+   });  
