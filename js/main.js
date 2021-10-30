800ec3dd047385897c7a86d021bb424ae508c72b
Shpilevskayaas <shpilevskayaas@ya.ru>
2021-10-30 18:01:16 +0300

    main__burger

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
    $(".header__toggle").click(function() {
      $(this).toggleClass("active");
-     $('.header__list').slideToggle(300, function(){
+     $('.header__list, .header__popup').slideToggle(300, function(){
        if($(this).css('display') === "none"){
          $(this).removeAttr('style');
        }
